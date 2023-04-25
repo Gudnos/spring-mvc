405a1be8c2c2b44e1444e29c6740f161caf88781
Anton Sirkin <gudnos@gmail.com>
2023-04-25 13:42:23 +0300

    Commit

--- src/main/java/service/ServiceCarImpl.java
+++ src/main/java/service/ServiceCarImpl.java
@@ -7,16 +7,21 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ServiceCarImpl implements ServiceCar{
+    private List<Car> cars;
+
+    {
+        cars = new ArrayList<>();
+
+        cars.add(new Car(1, "White", "BMW"));
+        cars.add(new Car(2, "Black", "Audi"));
+        cars.add(new Car(3, "Grey", "Ford"));
+        cars.add(new Car(4, "Red", "Nissan"));
+        cars.add(new Car(5, "Green", "Toyota"));
+    }
 
     @Override
     public List<Car> findCars(int number) {
-        List<Car> list = new ArrayList<>();
-        list.add(new Car(1, "White", "BMW"));
-        list.add(new Car(2, "Black", "Audi"));
-        list.add(new Car(3, "Grey", "Ford"));
-        list.add(new Car(4, "Red", "Nissan"));
-        list.add(new Car(5, "Green", "Toyota"));
-        if (number <= 0 || number > 5) return list;
-        return list.stream().limit(number).collect(Collectors.toList());
+        if (number <= 0 || number > 5) return cars;
+        return cars.stream().limit(number).collect(Collectors.toList());
     }
 }
--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,6 +1,5 @@
 package web.controller;
 
-import model.Car;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,16 +7,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import service.ServiceCar;
 import service.ServiceCarImpl;
 
-import java.util.List;
-
 @Controller
 public class CarController {
 
+    private ServiceCar sc = new ServiceCarImpl();
+
     @GetMapping(value = "/cars")
     public String printWelcome(@RequestParam(value = "count", defaultValue = "5") int count, ModelMap model) {
-        ServiceCar sc = new ServiceCarImpl();
-        List<Car> cars = sc.findCars(count);
-        model.addAttribute("cars", cars);
+        model.addAttribute("cars", sc.findCars(count));
         return "cars";
     }
 
